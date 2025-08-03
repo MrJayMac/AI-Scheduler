8d01a16af01df79c2e1dd36bbd1f995dfb25ebd3
Joshua Mac <mac.joshua450@gmail.com>
2025-08-03 14:08:09 -0400

    Adding supabase client

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,30 @@
+"use client"
+
+import { useState } from "react";
+import { supabase } from "../../client/supabase-client";
+
 export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleClick = () => {
+    supabase.auth.signInWithPassword({ email, password })
+  }
+
+
   return (
     <div>
-      hi
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleClick}>Login</button>
     </div>
   );
 }
